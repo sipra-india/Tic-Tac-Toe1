70ace3d17e52b8d05e7726a1f34c384d61212231
Sipra <dash.sipra.sanghamitra@gmail.com>
2023-11-27 15:06:46 +0530

    second commit

--- src/Board.jsx
+++ src/Board.jsx
@@ -9,18 +9,18 @@ export function Board(){
   return <div>
   <div className="row one">
     <Square val="1"/>
-    <Square val="1"/>
-    <Square val="1"/>
+    <Square val="2"/>
+    <Square val="3"/>
   </div>
   <div className="row two">
-    <Square val="1"/>
-    <Square val="1"/>
-    <Square val="1"/>
+    <Square val="4"/>
+    <Square val="5"/>
+    <Square val="6"/>
   </div>
   <div className="row three">
-    <Square val="1"/>
-    <Square val="1"/>
-    <Square val="1"/>
+    <Square val="7"/>
+    <Square val="8"/>
+    <Square val="9"/>
   </div>  
   </div>
 }
